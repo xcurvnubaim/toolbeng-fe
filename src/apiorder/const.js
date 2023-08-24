8ae9a12ae76a3854f9edb8d394fb59fb41b09900
Mochammad Zharif <zhariffmarzuqi@gmail.com>
2023-08-24 22:02:59 +0700

    test

--- src/apiorder/const.js
+++ src/apiorder/const.js
@@ -1,4 +1,4 @@
-// const API_BASE_URL = "https://amaranth-kingfisher-kit.cyclic.app"
-const API_BASE_URL = 'http://192.168.0.109:5000'
+const API_BASE_URL = "https://amaranth-kingfisher-kit.cyclic.app"
+// const API_BASE_URL = 'http://192.168.0.109:5000'
 
 export default API_BASE_URL;
--- src/context/socket.js
+++ src/context/socket.js
@@ -1,6 +1,7 @@
 "use client"
+import API_BASE_URL from '@/apiorder/const';
 import React from 'react';
 import {io} from 'socket.io-client'
 
-export const socket = io.connect("http://192.168.0.109:5000");
+export const socket = io.connect(API_BASE_URL);
 export const SocketContext = React.createContext(socket);
